4c6d0fa4c7c922b49bf38c455165968f2d37de14
varikuppalasampath <108409556+varikuppalasampath@users.noreply.github.com>
2023-06-16 17:45:07 +0530

    Create location_script.py

---
+++ location_script.py
@@ -0,0 +1,9 @@
+import geocoder
+
+def get_location():
+    g = geocoder.ip('me')
+    return g.latlng
+
+if __name__ == "__main__":
+    latitude, longitude = get_location()
+    print(f"Latitude: {latitude}, Longitude: {longitude}")
